f3e8a235a84af7e005eb706c05fc3cd90cb01be3
GreggC95 <43067498+GreggC95@users.noreply.github.com>
2018-09-21 16:12:22 +0100

    critters despawn when timer is 0

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,8 @@ public class GameManager : MonoBehaviour {
     public List<GameObject> critterPrefabs;
     public float critterSpawnFreqency = 1.0f;
     public Score scoreDisplay;
+    public Timer timer;
+
 
     private float lastCritterSpawn = 0;
 
@@ -21,7 +23,7 @@ public class GameManager : MonoBehaviour {
 
         //check if it time to spawn the next critter
         float nextSpawnTime = lastCritterSpawn + critterSpawnFreqency;
-        if (Time.time >= lastCritterSpawn + critterSpawnFreqency)
+        if (Time.time >= nextSpawnTime && timer.IsTimerRunning() == true)
         {
             // it is time!
 
@@ -35,8 +37,9 @@ public class GameManager : MonoBehaviour {
             // Get access to our Critter script
             Critter critterScript = spawnedCritter.GetComponent<Critter>();
 
-            // Tell the critter script the 
+            // Tell the critter script the score and timer objects
             critterScript.scoreDisplay = scoreDisplay;
+            critterScript.timer = timer;
 
             // update the most recent critter spawn time to now
             lastCritterSpawn = Time.time;
